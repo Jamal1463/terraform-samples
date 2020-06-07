ee5da4ec6234fb1c7d29803424249ff178ecf49b
Jamal1463 <mohd.jama412@gmail.com>
2020-06-07 21:29:50 +0530

    second commit for s2

---
+++ s2-vpc-demo/instance.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "example" {
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  count         = "3"
+  key_name      = "${aws_key_pair.redmikey.key_name}"
+  tags = {
+    Name = "TF-Server-${count.index + 1}"
+  }
+  #vpc subnets
+  subnet_id = "${aws_subnet.tf-public-1.id}"
+}
+
+resource "aws_key_pair" "redmikey" {
+  key_name   = "redmikey"
+  public_key = "${file(var.PATH_TO_PUBLIC_KEY)}"
+}
+

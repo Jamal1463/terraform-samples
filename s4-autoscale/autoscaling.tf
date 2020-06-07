e07b7732279cb9d9c5c94365ea819e7000db2447
Jamal1463 <mohd.jama412@gmail.com>
2020-06-07 21:40:48 +0530

    commit for autoscale

---
+++ s4-autoscale/autoscaling.tf
@@ -0,0 +1,23 @@
+resource "aws_launch_configuration" "example-launchconfig" {
+  name_prefix     = "example-launchconfig"
+  image_id        = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.allow-ssh.id}"]
+}
+
+resource "aws_autoscaling_group" "example-autoscaling" {
+  name                      = "example-autoscaling"
+  vpc_zone_identifier       = ["${aws_subnet.tf-public-1.id}", "${aws_subnet.tf-public-2.id}"]
+  launch_configuration      = "${aws_launch_configuration.example-launchconfig.id}"
+  min_size                  = 1
+  max_size                  = 2
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  force_delete              = "true"
+  tag {
+    key                 = "Name"
+    value               = "tf-as-server"
+    propagate_at_launch = "true"
+  }
+}
+
